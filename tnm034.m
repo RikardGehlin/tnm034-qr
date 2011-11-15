b9ef9796f96385ead5aa341678a8017c4eee84ca
Marcus Stenbeck <marcus.stenbeck@gmail.com>
2011-11-15 13:46:31 +0100

    Allokerade variablerna med delviktningsfunktionerna. Fixade lite syntaxfel.

--- tnm034.m
+++ tnm034.m
@@ -14,6 +14,13 @@ double_image = im2double(image); % turning the pixel-values into double-values
 normalized_image = double_image / max(double_image(:)); % normalizes between 0 and 1
 
 [width, height] = size(normalized_image);
+
+% pre-allocate variables to speed up the program
+T = zeros([width,height]);
+P = zeros([width,height]);
+Q = zeros([width,height]);
+B = zeros([width,height]);
+
 for x = 1 : width
     for y = 1 : height
         %gaussian highpass-filter för att ta fram kanter s.184 i BoB-boken
@@ -26,10 +33,10 @@ for x = 1 : width
         %y_axis = 7 värden ifrån (x,y) i y-led
         %r_axis = 7 värden ifrån (x,y) i r-led
         %s_axis = 7 värden ifrån (x,y) i s-led
-        T[x,y] = ( central_symmetry(x_axis) + central_symmetry(y_axis) + central_symmetry(r_axis) + central_symmetry(s_axis) )/4;
-        P[x,y] = ( ratio_characteristic(x_axis) + ratio_characteristic(y_axis) + ratio_characteristic(r_axis) + ratio_characteristic(s_axis) )/4;
-        Q[x,y] = ( square_characteristic(x_axis,y_axis) + square_characteristic(r_axis, s_axis) )/2;
-        B[x,y] = ( T[x,y] + P[x,y] + Q[x,y] )/3;
+        T(x,y) = ( central_symmetry(x_axis) + central_symmetry(y_axis) + central_symmetry(r_axis) + central_symmetry(s_axis) )/4;
+        P(x,y) = ( ratio_characteristic(x_axis) + ratio_characteristic(y_axis) + ratio_characteristic(r_axis) + ratio_characteristic(s_axis) )/4;
+        Q(x,y) = ( square_characteristic(x_axis,y_axis) + square_characteristic(r_axis, s_axis) )/2;
+        B(x,y) = ( T[x,y] + P[x,y] + Q[x,y] )/3;
         end
     end
 end
