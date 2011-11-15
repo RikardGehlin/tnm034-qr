29ca6bb35b9a9e41b1d5ac628f2ee84fb5221da2
Rikard Gehlin <rikardgehlin@gmail.com>
2011-11-15 08:36:32 +0100

    lagt till funktionen getinfo

---
+++ getinfo.m
@@ -0,0 +1,8 @@
+% DESCRIPTION
+%   A function that extracts the data from the supplied image
+
+% PARAMETERS
+%   IN:
+%     imin: The input image of the captured QR-code.
+%   OUT:
+%     strout: The resulting character string of the coded message. 
