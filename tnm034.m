83c673acd5dadd6a0abf367af121843259e04efd
Rikard Gehlin <rikardgehlin@gmail.com>
2011-11-19 18:05:40 +0100

    fail, på tnm034.m men verkar inte kunna ladda upp nåt annat annrs?

--- tnm034.m
+++ tnm034.m
@@ -1,3 +1,4 @@
+function strout = tnm034(imin)
 % DESCRIPTION
 %   A function that removes noise from the supplied image
 
@@ -8,30 +9,76 @@
 %     strout: The resulting character string of the coded message. 
 %     The string must be in a pre-defined format given in the course description.
 
-function strout = tnm034(imin)
-
 image = imread(imin); % reads the given image
 double_image = im2double(image); % turning the pixel-values into double-values
 normalized_image = double_image / max(double_image(:)); % normalizes between 0 and 1
 
-[width, height] = size(normalized_image);
-for x = 1 : width
-    for y = 1 : height
-        %gaussian highpass-filter för att ta fram kanter
-        gauss[x,y] = ((x-(width/2))^2 + (y-(height/2))^2)^(1/2);
-        filtrerad[x,y] = 1 - exp(-gauss[x,y]^2/(2*gauss[0,0]^2)); 
-        
-        if(normalized_image(x,y) < 0,5) %mät ut punkter, sätt in i respektive vektor
+[height, width] = size(normalized_image);
+
+% pre-allocate variables to speed up the program
+T = zeros(height,width);
+P = zeros(height,width);
+Q = zeros(height,width);
+B = zeros(height,width);
+
+A = 0.15;
+sigma = 20;
+
+new = filter2(1-fspecial('gaussian'), normalized_image, 'same');
+figure(18);
+imshow(new);
+
+fourierbild = fft2(new);
+shiftad = fftshift(fourierbild);
+[r, c] = size(shiftad);
+[u, v] = meshgrid(-(c/2)+1:1:c/2, -(r/2)+1:1:r/2);
+gauss = exp(-((u.^2+v.^2)/(2*sigma^2)));
+
+filtrering = gauss.*shiftad; %Gör så att du filtrerar originalbilden med lp-filtret
+x = ifftshift(filtrering); %skiftar tillbaka det
+resultat = ifft2(x); %transformerar tillbaka till spatialdomänen
+imshow(resultat);
+
+extra = normalized_image - resultat; % bara kontroll vad det är vi har gjort!
+figure(2);
+imshow(extra);
+
+sharp = normalized_image + B.*(normalized_image - resultat); %skärpning = bild + kantlinjer
+figure(3);
+imshow(sharp);
+figure(4);
+imshow(normalized_image);
+
+%filtrering = gauss.*shiftad; %Gör så att du filtrerar originalbilden med lp-filtret
+%x = ifftshift(filtrering); %skiftar tillbaka det
+%resultat = ifft2(x); %transformerar tillbaka till spatialdomänen
+%figure(3);
+%imshow(resultat);
+%for x = 1 : width
+ %   for y = 1 : height
+  %     if(normalized_clean_image(y,x) <= 0.5) %mät ut punkter, sätt in i respektive vektor
+   %        normalized_clean_image(y,x) = 0;
+    %   else
+     %      normalized_clean_image(y,x) = 1;
+      % end
+       
         %x_axis = 7 värden ifrån (x,y) i x-led där x[0]= (x,y)
+   %     x_axis[0] = clean_image(y,x);
         %y_axis = 7 värden ifrån (x,y) i y-led
+    %    y_axis[0] = clean_image(y,x);
         %r_axis = 7 värden ifrån (x,y) i r-led
+        %x_axis[0] = normalized_image(x,y);
         %s_axis = 7 värden ifrån (x,y) i s-led
-        T[x,y] = ( central_symmetry(x_axis) + central_symmetry(y_axis) + central_symmetry(r_axis) + central_symmetry(s_axis) )/4;
-        P[x,y] = ( ratio_characteristic(x_axis) + ratio_characteristic(y_axis) + ratio_characteristic(r_axis) + ratio_characteristic(s_axis) )/4;
-        Q[x,y] = ( square_characteristic(x_axis,y_axis) + square_characteristic(r_axis, s_axis) )/2;
-        B[x,y] = ( T[x,y] + P[x,y] + Q[x,y] )/3;
-    end
-end
+        %x_axis[0] = normalized_image(x,y);
+     %   T(y,x) = ( central_symmetry(x_axis) + central_symmetry(y_axis) + central_symmetry(r_axis) + central_symmetry(s_axis) )/4;
+      %  P(y,x) = ( ratio_characteristic(x_axis) + ratio_characteristic(y_axis) + ratio_characteristic(r_axis) + ratio_characteristic(s_axis) )/4;
+       % Q(y,x) = ( square_characteristic(x_axis,y_axis) + square_characteristic(r_axis, s_axis) )/2;
+        %B(y,x) = ( T[y,x] + P[y,x] + Q[y,x] )/3;
+      %  end
+   % end
+%end
+%figure(4);
+ %      imshow(normalized_clean_image);
 %localization(normalized_image);
 
 %noise(normalized_image); % calls a function to remove all the noise in the picture
