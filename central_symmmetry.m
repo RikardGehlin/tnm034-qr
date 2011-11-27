daa217892de3008a7c4318c13f726f403544e529
Rikard Gehlin <rikardgehlin@gmail.com>
2011-11-27 14:19:00 +0100

    uppdaterat viktningsfunktionerna så att man bara skickar in vektorer, och att de returnerar ett viktningsvärde också

--- central_symmmetry.m
+++ central_symmmetry.m
@@ -3,7 +3,6 @@
 
 % PARAMETERS
 %   IN:
-%     o: The piaxisel currently being evaluated.
 %     axis: The axis being evaluated 
 %   OUT:
 %     int: An integer which will be weighted later
@@ -12,4 +11,4 @@ function int = central_symmetri(axis)
 
 int = (1/3)*(abs((abs(axis[4]-axis[0])-abs(axis[0]-axis[3]))/abs(axis[4]-axis[3])) + abs((abs(axis[5]-axis[0])-abs(axis[0]-axis[2]))/abs(axis[5]-axis[2])) + abs((abs(axis[6]-axis[0])-abs(axis[0]-axis[1]))/abs(axis[6]-axis[1])));
 
-return;
+return int;
--- ratio_characteristic.m
+++ ratio_characteristic.m
@@ -3,13 +3,12 @@
 
 % PARAMETERS
 %   IN:
-%     o: The pixel currently being evaluated.
 %     axis: The axis being evaluated 
 %   OUT:
 %     int: An integer which will be weighted later
 
-function int = ratio_characteristic(o, axis)
+function int = ratio_characteristic(axis)
 
 int = (1/7)*(abs((axis[5]-axis[1])/(axis[6]-axis[2]) - 1) + abs((axis[4]-axis[1])/(axis[6]-axis[3]) - 1) + abs((axis[3]-axis[1])/(axis[6]-axis[4]) - 1) + abs((axis[5]-axis[3])/(axis[6]-axis[4]) - 1) + abs((axis[4]-axis[2])/(axis[3]-axis[1]) - 1) + abs((axis[3]-axis[2])/(axis[5]-axis[4]) - 1) + abs((axis[2]-axis[1])/(axis[6]-axis[5]) - 1));
 
-return;
+return int;
